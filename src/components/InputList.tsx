abe426a7f08b7b2f8a927a483e44ff1bd65dc177
Austin Schey <aschey13@gmail.com>
2023-10-07 11:11:21 -0500

    add/remove animation

--- src/components/InputList.tsx
+++ src/components/InputList.tsx
@@ -1,8 +1,9 @@
-import { Component, For } from "solid-js";
+import { Component, For, createSignal, onMount } from "solid-js";
 import { Input } from "./Input";
 import Fa from "solid-fa";
 import { faPlus, faX } from "@fortawesome/free-solid-svg-icons";
 import { Button } from "./Button";
+import { twMerge } from "tailwind-merge";
 
 export interface InputListItem {
   name: (index: number) => string;
@@ -20,7 +21,7 @@ export interface InputListProps {
 
 export const InputList: Component<InputListProps> = (props) => {
   return (
-    <div class="flex flex-col">
+    <div class="flex flex-col transition-all">
       <div class="flex m-3 w-full items-center justify-between">
         <div class="flex">Test5</div>
         <div class="flex mr-1">
@@ -32,27 +33,70 @@ export const InputList: Component<InputListProps> = (props) => {
 
       <For each={props.items}>
         {(item, i) => (
-          <div class="flex w-full relative h-14 overflow-hidden">
-            <div class="flex w-full absolute top-0 left-0 opacity-100">
-              <div class="flex">
-                <Button
-                  outlined
-                  class="mx-3 h-8 my-3 text-error"
-                  onClick={() => props.onRemove(item, i())}
-                >
-                  <Fa icon={faX} />
-                </Button>
-              </div>
-              <Input
-                type={props.type}
-                name={item.name(i())}
-                label={item.label?.(i())}
-                fullWidth={props.fullWidth}
-              />
-            </div>
-          </div>
+          <InputListItem
+            item={item}
+            index={i()}
+            type={props.type}
+            fullWidth={props.fullWidth}
+            onRemove={props.onRemove}
+          />
         )}
       </For>
+    </div>
+  );
+};
+
+interface InputListItemProps {
+  item: InputListItem;
+  index: number;
+  type: "number" | "text" | "checkbox";
+  fullWidth?: boolean;
+  onRemove: (item: InputListItem, index: number) => void;
+}
+
+const InputListItem: Component<InputListItemProps> = (props) => {
+  const [show, setShow] = createSignal(false);
+
+  onMount(() => {
+    setTimeout(() => {
+      setShow(true);
+    }, 15);
+  });
+
+  return (
+    <div
+      class={twMerge(
+        "flex w-full relative  overflow-hidden transition-all",
+        show() ? "h-14" : "h-0"
+      )}
+    >
+      <div
+        class={twMerge(
+          "flex w-full absolute top-0 left-0 transition-all",
+          show() ? "opacity-100" : "opacity-0"
+        )}
+      >
+        <div class="flex">
+          <Button
+            outlined
+            class="mx-3 h-8 my-3 text-error"
+            onClick={() => {
+              setShow(false);
+              setTimeout(() => {
+                props.onRemove(props.item, props.index);
+              }, 150);
+            }}
+          >
+            <Fa icon={faX} />
+          </Button>
+        </div>
+        <Input
+          type={props.type}
+          name={props.item.name(props.index)}
+          label={props.item.label?.(props.index)}
+          fullWidth={props.fullWidth}
+        />
+      </div>
     </div>
   );
 };
